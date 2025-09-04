ebd53c1037759a744c7a1362068cead75359b8a6
student02 <dgdhgfjkl@rdgfh.com>
2025-09-04 13:36:22 +0200

    commit

--- app/src/main/java/com/example/wakacje/MainActivity.java
+++ app/src/main/java/com/example/wakacje/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.wakacje;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,6 +17,7 @@ import java.time.temporal.ChronoUnit;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,16 +30,29 @@ public class MainActivity extends AppCompatActivity {
         });
         EditText editText = findViewById(R.id.daysToEnd);
         TextView textView = findViewById(R.id.holydaysStart);
+        TextView holidaysTime = findViewById(R.id.holidaysTime);
 
         LocalDate holidaysStarts = lastSaturdayOfJune(LocalDate.now().getYear());
+        LocalDate schoolStarts = firstDayOfSchool(LocalDate.now().getYear());
 //        jeżeli moja data jest po starcie wakacji(obliczamy przyszłe wakacje)
         if(LocalDate.now().isAfter(holidaysStarts)){
             holidaysStarts = lastSaturdayOfJune(LocalDate.now().getYear()+1);
         }
-        textView.setText(String.format("Wakacje zaczynają się %s", holidaysStarts.toString()));
-        long days = ChronoUnit.DAYS.between(LocalDate.now(), holidaysStarts);
-        editText.setText(String.valueOf(days + " dni"));
+        if(LocalDate.now().isAfter(schoolStarts)){
+            schoolStarts = firstDayOfSchool(LocalDate.now().getYear()+1);
+        }
+
+        if(LocalDate.now().isAfter(holidaysStarts) && LocalDate.now().isBefore(schoolStarts)){
+            textView.setText("są wakacje");
+            editText.setText("0");
+        }else{
+            textView.setText(String.format("Wakacje zaczynają się %s", holidaysStarts.toString()));
+            long days = ChronoUnit.DAYS.between(LocalDate.now(), holidaysStarts);
+            editText.setText(String.valueOf(days + " dni"));
+        }
+        holidaysTime.setText(String.format("Wakacje od %s do %s", holidaysStarts.toString(), schoolStarts.toString()));
     }
+
     private LocalDate lastSaturdayOfJune(int year){
         LocalDate endOfJune = LocalDate.of(year, 6, 30);
         while(endOfJune.getDayOfWeek() != DayOfWeek.SATURDAY){
@@ -45,4 +60,15 @@ public class MainActivity extends AppCompatActivity {
         }
         return endOfJune;
     }
+    private LocalDate firstDayOfSchool(int year){
+        LocalDate firstOfSeptember = LocalDate.of(year,9,1);
+        while(firstOfSeptember.getDayOfWeek() == DayOfWeek.SATURDAY){
+            firstOfSeptember = firstOfSeptember.plusDays(2);
+        }
+        while(firstOfSeptember.getDayOfWeek() == DayOfWeek.SUNDAY){
+            firstOfSeptember = firstOfSeptember.plusDays(1);
+        }
+        return firstOfSeptember;
+    }
+//    jak bys sie kiedys z tego uczył to troche zepsułem xd
 }
